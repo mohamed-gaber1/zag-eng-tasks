8928aec032424b04fc2889e916f655bb2f39d560
mohamed-gaber1 <m.gaber.fouda123@gmail.com>
2026-09-02 20:28:49 +0300

    Delete Task 4 directory

--- Task 4/middleware/c.log.middleware.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response,NextFunction } from "express";
-const logger=(req:Request,res:Response,next:NextFunction)=>{
-    const myDate= new Date().toString();
-    console.log(`[${myDate}] ${req.method} ${req.originalUrl}`)
-    next();
-}
-module.exports=logger;
